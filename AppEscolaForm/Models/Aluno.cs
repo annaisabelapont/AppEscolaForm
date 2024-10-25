0bc0037e7ccb15aa339e375c0366c2b614cebaae
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2024-10-25 15:50:54 -0400

    first commit

---
+++ AppEscolaForm/Models/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEscolaForm.Models
+{
+    public class Aluno
+    {
+        //atributos
+        public int Id { get; set; }
+        public int NumMatricula { get; set; }
+        public string Nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public int IdSalaDeAula { get; set; }
+
+        //métodos 
+        public double CalcularMedia()
+        {
+            return (Nota1 + Nota2) / 2;
+        }
+
+        public string VerificarSituacao()
+        {
+            var situacao = "REPROVADO(A)";
+            if (CalcularMedia() >= 60) situacao = "APROVADO(A)";
+            return situacao;
+        }
+    }
+}
---
+++ AppEscolaForm/Models/SalaDeAula.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEscolaForm.Models
+{
+    public class SalaDeAula
+    {
+        public int Id { get; set; }
+        public int Serie { get; set; }
+        public string NomeTurma { get; set; }
+        public int Ano { get; set; }
+    }
+}
