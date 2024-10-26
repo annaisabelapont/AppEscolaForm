08bd9a44448a5a8a4daf1078501e9cd597ca633b
giovrds <escolargiovanna@gmail.com>
2024-10-26 13:58:38 -0400

    feat: botao seu pc foi infectado

--- AppEscolaForm/Formularios/CadastrarSalaDeAula.cs
+++ AppEscolaForm/Formularios/CadastrarSalaDeAula.cs
@@ -16,5 +16,10 @@ namespace AppEscolaForm.Formularios
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("seu pc foi infectado", "a", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+        }
     }
 }
